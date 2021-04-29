688f67b5051df79a52ef6c3de195f4f5bf40579d
Stephanie Gilmore <74026523+skgilmore@users.noreply.github.com>
2021-04-29 15:18:13 -0500

    Update 01_Db_Create.sql

--- 01_Db_Create.sql
+++ 01_Db_Create.sql
@@ -1,36 +1,9 @@
-CREATE TABLE [User] (
-  [id] int PRIMARY KEY,
-  [name] nvarchar(255) NOT NULL,
-  [email] nvarchar(255) NOT NULL,
-  [userName] nvarchar(255) NOT NULL,
-  [zip] int NOT NULL,
-  [fireBaseUserId] nvarchar(255) NOT NULL
-)
 GO
-
-CREATE TABLE [Route] (
-  [id] int PRIMARY KEY,
-  [name] nvarchar(255) NOT NULL,
-  [difficultyLevel] nvarchar(255),
-  [length] nvarchar(255),
-  [recAreaId] int,
-  [ammenityId] int
-)
-GO
-
 CREATE TABLE [Ammenity] (
   [id] int PRIMARY KEY,
   [label] nvarchar(255)
 )
 GO
-
-CREATE TABLE [Favorite] (
-  [id] int,
-  [userId] int,
-  [routeId] int
-)
-GO
-
 CREATE TABLE [RecArea] (
   [id] int PRIMARY KEY,
   [name] nvarchar(255) NOT NULL,
@@ -42,7 +15,30 @@ CREATE TABLE [RecArea] (
   [url] nvarchar(255)
 )
 GO
+CREATE TABLE [User] (
+  [id] int PRIMARY KEY,
+  [name] nvarchar(255) NOT NULL,
+  [email] nvarchar(255) NOT NULL,
+  [userName] nvarchar(255) NOT NULL,
+  [zip] int NOT NULL,
+)
+GO
+CREATE TABLE [Route] (
+  [id] int PRIMARY KEY,
+  [name] nvarchar(255) NOT NULL,
+  [difficultyLevel] nvarchar(255),
+  [length] nvarchar(255),
+  [recAreaId] int,
+  [ammenityId] int
+)
+GO
 
+CREATE TABLE [Favorite] (
+  [id] int,
+  [userId] int,
+  [routeId] int
+)
+GO
 CREATE TABLE [Review] (
   [id] int PRIMARY KEY,
   [userId] int NOT NULL,
